bbafd131d71c0f86be26b2d16e5f36d5a728ab44
Fourty3000 <loic.pierre.sannier@gmail.com>
2026-05-30 15:47:47 +0200

    Refactor le script de démarrage pour utiliser le serveur personnalisé et ajouter un fichier de serveur

---
+++ server/serve.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const next = require('next')
+
+const port = Number(process.env.PORT || 8580)
+const hostname = process.env.HOSTNAME || '0.0.0.0'
+const dev = process.env.NODE_ENV !== 'production'
+
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+
+app
+  .prepare()
+  .then(() => {
+    http
+      .createServer((req, res) => {
+        handle(req, res)
+      })
+      .listen(port, hostname, () => {
+        console.log(`Server ready on http://${hostname}:${port}`)
+      })
+  })
+  .catch((error) => {
+    console.error('Failed to start server:', error)
+    process.exit(1)
+  })
